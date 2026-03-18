46f7b9aa63b691ac0465d65a3b84a4f01493ac79
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-03-18 09:53:54 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,28 @@
+let matriz = [
+    ['catatau', 45, 78],
+    ['sapato', 5, 6],
+    ['maneiro', 8, 9, 34]
+]
+
+/*for(let i =0; i<matriz.length; i++){
+    for(let j=0; j<matriz[i].length; j++){
+        console.log(matriz[i][j]);
+    }
+} */
+
+
+
+
+
+//verificar quantos itens tem na matriz
+
+quantidade = 0
+
+for(let i =0; i<matriz.length; i++){
+    for(let j=0; j<matriz[i].length; j++){
+        quantidade++;
+        
+    }
+}
+
+// console.log(`Sua matriz tem ${quantidade} itens.`)
